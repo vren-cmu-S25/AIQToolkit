17f6d057445bb46623388788116d349ebb208c45
Vincent Ren <vren@andrew.cmu.edu>
2025-05-21 14:08:32 -0700

    Memory agent

--- company_financial_mas/src/company_financial_mas/register.py
+++ company_financial_mas/src/company_financial_mas/register.py
@@ -11,3 +11,5 @@ This file imports all modules that register functions with AIQ.
 
 # Import any modules which need to be automatically registered here
 from . import profitability_analysis 
+from . import retrieve_data_tool
+from . import update_data_tool
